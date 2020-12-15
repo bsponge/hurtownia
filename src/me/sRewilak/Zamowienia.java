1a2fec76b907c76e00331164b68f0d1b5cd7fe8d
Szymon Rewilak <szymonrewilak@gmail.com>
2020-12-15 16:29:38 +0100

    Dodano prototyp klasy Zamowienia

---
+++ src/me/sRewilak/Zamowienia.java
@@ -0,0 +1,29 @@
+package me.sRewilak;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Zamowienia {
+     private List<Zamowienie> listaZamowien;
+
+     public Zamowienia(){
+         listaZamowien = new LinkedList<>();
+     }
+
+     public void realizujZamowienie(String idPracownika, String idZamowienia){
+
+     }
+
+     public void wyswietlZamowienia(String idPracownika){
+
+     }
+
+     public void dodajZamowienie(Zamowienie zamowienie){
+
+     }
+
+     public void usunZamowienie(String idZamowienia){
+         
+     }
+
+}
