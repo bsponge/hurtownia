e5fd09baaa6a30281fc14b9623ad456648355c93
Vark123 <vark123@interia.pl>
2021-01-04 15:37:48 +0100

    Druga próba commitu

---
+++ test/MagazynTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MagazynTest {
+
+	@Test
+	void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetIlosc() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/ProduktTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProduktTest {
+
+	@Test
+	void testGetNazwa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCenaJednostkowa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProducent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetJednostka() {
+		fail("Not yet implemented");
+	}
+
+}
