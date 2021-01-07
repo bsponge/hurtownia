fc651ef5f45bda52cdcb289b243148c3b25997b4
bsponge <bsponge@github.com>
2021-01-07 23:12:35 +0100

    Dodano komentarze javadoc

--- src/me/jSkiba/Koszyk.java
+++ src/me/jSkiba/Koszyk.java
@@ -6,13 +6,26 @@ import me.bRosiak.Produkt;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Reprezentuje koszyk klienta
+ * @author Jakub Skiba
+ */
 public class Koszyk {
     private Map<Produkt, Integer> produkty;
 
+    /**
+     * Tworzy pusty koszyk
+     */
     public Koszyk() {
         produkty = new ConcurrentHashMap<>();
     }
 
+    /**
+     * Zwraca mape produktow
+     * KLUCZ : Produkt
+     * WARTOSC : Ilosc produktow
+     * @return Mapa z kluczem klasy Produkt i wartoscia klasy Integer reprezentujaca koszyk klienta
+     */
     public Map<Produkt, Integer> getProdukty() {
         return produkty;
     }
@@ -20,6 +33,12 @@ public class Koszyk {
     /*
     Metoda dodaj i usun produkt musza sprawdzac dostepnosc produktow
     w magazynie i zmieniac stan mapy produktow w koszyku
+     */
+
+    /**
+     * Dodaje produkt do koszyka w podanej liczbie jednostek
+     * @param produkt Produkt, ktory ma zostac dodany do koszyka
+     * @param ilosc Ilosc jednostek produktu
      */
     public void dodajProdukt(Produkt produkt, int ilosc) {
         Magazyn magazyn = Magazyn.getInstance();
@@ -32,17 +51,10 @@ public class Koszyk {
         }
     }
 
-    public void zmienIloscProduktow(Produkt produkt, int ilosc) {
-        Magazyn magazyn = Magazyn.getInstance();
-        if (magazyn != null) {
-            if (produkt != null) {
-                if (magazyn.getIlosc(produkt) >= ilosc) {
-                    produkty.replace(produkt, ilosc);
-                }
-            }
-        }
-    }
-
+    /**
+     * Usuwa produkt z koszyka
+     * @param produkt Produkt, ktory ma zostac usuniety z koszyka
+     */
     public void usunProdukt(Produkt produkt) {
         produkty.remove(produkt);
     }
--- src/me/jSkiba/Osoba.java
+++ src/me/jSkiba/Osoba.java
@@ -1,20 +1,37 @@
 package me.jSkiba;
 
 // klasa bazowa dla klasy Klient i Pracownik
+
+/**
+ * Reprezentuej osobe
+ * @author Jakub Skiba
+ */
 public class Osoba {
     private final String imie;
     private final String nazwisko;
 
+    /**
+     * Tworzy osobe z podanym imieniem i nazwiskiem
+     * @param imie Imie osoby
+     * @param nazwisko Nazwisko osoby
+     */
     public Osoba(String imie, String nazwisko) {
         this.imie = imie;
         this.nazwisko = nazwisko;
     }
 
-    //GETTERY
+    /**
+     * Zwraca imie osoby
+     * @return String reprezentujacy imie osoby
+     */
     public String getImie() {
         return imie;
     }
 
+    /**
+     * Zwraca nazwisko osoby
+     * @return String reprezentujacy nazwisko osoby
+     */
     public String getNazwisko() {
         return nazwisko;
     }
