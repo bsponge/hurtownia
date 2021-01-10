9406f2f5af9828f50e7603e6c51a7b1636e36c35
bsponge <bsponge@github.com>
2021-01-10 20:08:09 +0100

    Dodano komentarze javadoc w klasie Hurtownia

--- src/me/jSkiba/Hurtownia.java
+++ src/me/jSkiba/Hurtownia.java
@@ -2,15 +2,27 @@ package me.jSkiba;
 
 import java.util.Scanner;
 
+/**
+ * Klasa odpowiedzialna za logike dzialania programu
+ */
 public class Hurtownia {
     public static Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Konstruktor klasy Hurtownia
+     */
     public Hurtownia() {}
 
+    /**
+     * Metoda odpowiedzialna za dzialania programu i odbieranie danych od uzytkownika
+     */
     public void run() {
 
     }
 
+    /**
+     * Wyswietla menu uzytkownika
+     */
     public void printMenu() {
         System.out.println("=====Menu=====");
         System.out.println("1.");
@@ -19,6 +31,10 @@ public class Hurtownia {
         System.out.println("0. Exit");
     }
 
+    /**
+     * Pobiera dane z wejscia tak dlugo az bedzie to integer
+     * @return Zwraca podany integer z wejscia lub -1 jezeli dane z wejscia nie moga byc przekonwertowane na integera
+     */
     public int getInput() {
         String str = null;
         try {
