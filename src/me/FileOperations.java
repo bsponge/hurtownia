76d8f4b9c3f8d841ebcd3464d5ddd83751f14973
bsponge <bsponge@github.com>
2021-01-11 13:17:12 +0100

    Dodano komentarze javadoc do klasy FileOperations

--- src/me/FileOperations.java
+++ src/me/FileOperations.java
@@ -2,7 +2,16 @@ package me;
 
 import java.io.*;
 
+/**
+ * Ulatwia zapisywanie i wczytywanie obiektow klas impelementujacych interfejs Serializable z plikow
+ */
 public class FileOperations {
+    /**
+     * Metoda statyczna. Zapisuje obiekt w pliku o nazwie nazwaPliku
+     * @param obiekt Obiekt ktory ma zostac zapisany w pliku. Musi implementowac interfejs Serializable
+     * @param nazwaPliku Nazwa pliku w ktorym zapisany ma zostac obiekt
+     * @param <T> Typ zapisywanego obiektu
+     */
     public static <T> void zapiszObiekt(T obiekt, String nazwaPliku) {
         File file = new File(nazwaPliku);
         try (FileOutputStream fileOutputStream = new FileOutputStream(file);
@@ -13,6 +22,13 @@ public class FileOperations {
         }
     }
 
+    /**
+     * Metoda statyczna. Odczytuje obiekt danego typu z pliku o nazwie nazwaPliku
+     * @param cls Uzywany do rzutowania obiektu pobranego z pliku np. jesli pobierany jest String to argument cls powinien byc String.class
+     * @param nazwaPliku Nazwa pliku, z ktorego pobrany ma zostac obiekt
+     * @param <T> Typ obiektu zapisanego w pliku oraz typ zwracany przez metode
+     * @return Obiekt przechowywany w pliku typu T lub null jezeli wystapi wyjatek
+     */
     public static <T> T odczytajObiekt(Class<T> cls, String nazwaPliku) {
         File file = new File(nazwaPliku);
         try (FileInputStream fileInputStream = new FileInputStream(file);
